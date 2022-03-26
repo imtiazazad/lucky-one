17cbb598a7fb8e92e77ae001f8276edfea853694
Imtiaz Azad <imtiaz16-549@diu.edu.bd>
2022-03-26 15:36:34 +0600

    linkkify e-commerce

---
+++ src/components/Header/Header.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import './Header.css'
+
+const Header = () => {
+    return (
+        <div className='header-text'>
+            <h1>Linkkify</h1>
+            <h3>IT Solutions</h3>
+        </div>
+    );
+};
+
+export default Header;
